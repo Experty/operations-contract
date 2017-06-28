e4b37d674daab05f3aeccef4c0dd687fac7aff02
Grzegorz Kućmierz <gkucmierz@gmail.com>
2017-06-28 23:37:17 +0200

    operations contract

---
+++ contracts/Operations.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.4;
+
+
+contract Operations {
+
+  mapping (address => uint) balances;
+
+  // mapping(address => mapping (address => uint256)) allowed;
+
+  function Operations() {
+    
+  }
+
+  function deposit() payable {
+    balances[msg.sender] = msg.value;
+  }
+
+  function withdraw(uint value) {
+    uint amount = balances[msg.sender];
+    
+    if (value < amount) {
+      amount = value;
+    }
+    
+    balances[msg.sender] -= amount;
+    msg.sender.transfer(amount);
+  }
+
+  function startCall() {
+
+  }
+
+  function endCall() {
+
+  }
+
+}

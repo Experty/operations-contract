fd1151451390382214c553a56c48ef758cbeebea
Grzegorz Kućmierz <gkucmierz@gmail.com>
2017-07-03 17:41:36 +0200

    added test placeholder

--- test/operations.js
+++ test/operations.js
@@ -2,12 +2,16 @@ const Operations = artifacts.require('./Operations')
 
 contract('Operations', accounts => {
   it('should deposit 1000 coins in the first account', async () => {
-    const instance = await Operations.deployed()
+    const instance = await Operations.deployed();
     const balanceBeforeDeposit = await instance.getBalance.call(accounts[0]).then(balance => balance.toNumber())
     instance
       .deposit({ value: 1000 })
       .then(() => instance.getBalance.call(accounts[0]))
       .then(balanceAfterDepositObj => balanceAfterDepositObj.toNumber())
       .then(balanceAfterDeposit => assert.equal(balanceAfterDeposit, balanceBeforeDeposit + 1000))
-  })
+  });
+
+  it('should not allow withdraw more than deposited value', async () => {
+    // …
+  });
 })

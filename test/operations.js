252af5275c682475e340519e120b2fb935a8825d
Daniel Kanas <dkanas95@gmail.com>
2017-07-04 11:40:25 +0200

    test for trying to withdraw more than the deposited value

--- test/operations.js
+++ test/operations.js
@@ -1,17 +1,22 @@
 const Operations = artifacts.require('./Operations')
 
 contract('Operations', accounts => {
-  it('should deposit 1000 coins in the first account', async () => {
-    const instance = await Operations.deployed();
-    const balanceBeforeDeposit = await instance.getBalance.call(accounts[0]).then(balance => balance.toNumber())
+  let instance = null
 
+  beforeEach(async () => {
+    instance = await Operations.deployed()
+  })
+
+  afterEach(() => instance = null)
+
+  it('should deposit 1000 coins in the first account', async () => {
+    const balanceBeforeDeposit = await instance.getBalance.call(accounts[0]).then(balance => balance.toNumber())
     await instance.deposit({ value: 1000 })
     const balanceAfterDeposit = await instance.getBalance.call(accounts[0]).then(balance => balance.toNumber())
     assert.equal(balanceAfterDeposit, balanceBeforeDeposit + 1000)
   })
 
   it('should withdraw the requested amount', async () => {
-    const instance = await Operations.deployed()
     await instance.deposit({ value: 1000 })
     await instance.withdraw(500)
     const balanceAfterWithdrawal = await instance.getBalance.call(accounts[0]).then(balance => balance.toNumber())
@@ -19,6 +24,7 @@ contract('Operations', accounts => {
   })
 
   it('should not allow withdraw more than deposited value', async () => {
-    // …
-  });
+    await instance.deposit({ value: 500 })
+    instance.withdraw(1000).catch(() => assert(true))
+  })
 })

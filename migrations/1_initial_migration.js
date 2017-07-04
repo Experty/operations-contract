f8f41bda924f4d00670d7b7d5d66348b4a0d6de5
Daniel Kanas <dkanas95@gmail.com>
2017-07-04 17:48:03 +0200

    unlocking the first account when using the dev network

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,6 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network) {
+  if (network === "development") web3.personal.unlockAccount(web3.eth.accounts[0], "");
   deployer.deploy(Migrations);
 };

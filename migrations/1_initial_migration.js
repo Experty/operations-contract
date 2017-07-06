f5555127780b2f766b5f1e728cf76f88434680e3
Daniel Kanas <dkanas95@gmail.com>
2017-07-06 15:00:53 +0200

    fixed test crashing on testrpc

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,6 +1,11 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer, network) {
-  if (network === "development") web3.personal.unlockAccount(web3.eth.accounts[0], "");
+  if (network === "development") {
+    try {
+      // try to unlock account for deploying the contracts
+      web3.personal.unlockAccount(web3.eth.accounts[0], "");
+    } catch(e) {}
+  }
   deployer.deploy(Migrations);
 };

c2d6de11b449d73cbf35287c57b7a23e3e314622
gkucmierz <gkucmierz@gmail.com>
2017-07-11 12:28:15 +0200

    added ec2 network

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ec2: {
+      host: "ec2-52-28-244-15.eu-central-1.compute.amazonaws.com",
+      port: 8545,
+      network_id: "*"
     }
   }
 };
